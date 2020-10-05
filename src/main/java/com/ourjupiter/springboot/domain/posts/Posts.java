0623160e1e87dc70c0f92c825726b8d049de80b9
boorim98 <bhuyugirl@gmail.com>
2020-10-05 15:02:58 +0900

    Add: crud & mysql 연동

---
+++ src/main/java/com/ourjupiter/springboot/domain/posts/Posts.java
@@ -0,0 +1,40 @@
+package com.ourjupiter.springboot.domain.posts;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import com.ourjupiter.springboot.domain.BaseTimeEntity;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Posts extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
